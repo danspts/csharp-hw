fb8ad796f0cbbb986d3aef42e91c752893504214
liavt <liavt@users.noreply.github.com>
2021-03-27 17:13:55 +0300

    started working on it

---
+++ ex1/DN_IDC_2021B_Ex01/B21 Ex01 LiavTurkia 323081950 DanBarak 227367455/B21_Ex01_2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace B21_Ex01_2
+{
+	public class Program
+	{
+		static void printStars(int n, int width) {
+			System.Console.Write(new System.String(' ', (width - n) / 2));
+			System.Console.Write(new System.String('*', n));
+			System.Console.WriteLine(new System.String(' ', (width - n) / 2));
+		}
+
+		static void recurseDown(int n, int height) {
+			if (n == 1)
+			{
+				recurseUp(1, height);
+			}
+			else
+			{
+				printStars(n, height);
+				recurseDown(n - 2, height);
+			}
+		}
+
+		static void recurseUp(int n, int height) {
+			printStars(n, height);
+			if (n < height) {
+				recurseUp(n + 2, height);
+			}
+		}
+
+		public static void printSandClock(int height) {
+			if (height % 2 == 0)
+			{
+				recurseDown(height + 1, height + 1);
+			}
+			else {
+				recurseDown(height, height);
+			}
+		}
+
+		static void Main()
+		{
+			printSandClock(5);
+		}
+	}
+}
---
+++ ex1/DN_IDC_2021B_Ex01/B21 Ex01 LiavTurkia 323081950 DanBarak 227367455/B21_Ex01_3/Program1.cs
@@ -0,0 +1,20 @@
+﻿namespace B21_Ex01_3
+{
+	public class Program
+	{
+		static void Main()
+		{
+			System.Console.WriteLine("Please insert the height of your desired sand clock:");
+			string input = System.Console.ReadLine();
+			try
+			{
+				int height = int.Parse(input);
+				// recurseDown handles even case
+				B21_Ex01_2.Program.printSandClock(height);
+			}
+			catch (System.ArgumentException e) {
+				System.Console.WriteLine("Invalid input!");
+			}
+		}
+	}
+}
