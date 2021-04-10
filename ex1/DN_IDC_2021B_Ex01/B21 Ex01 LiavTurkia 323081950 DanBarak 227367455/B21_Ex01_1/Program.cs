d0f2946f2d758e6f2242f6bb1e46b3bb6798ffbb
Dan Barak Sportes <danspts@gmail.com>
2021-04-10 18:59:22 +0300

    * Program.cs: * Program.cs: * B21_Ex01_5.csproj: * B21_Ex01_1.csproj:
    * B21 Ex01 Liav 323081950 Dan 227367455.sln: added ex 5

---
+++ ex1/DN_IDC_2021B_Ex01/B21 Ex01 LiavTurkia 323081950 DanBarak 227367455/B21_Ex01_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B21_Ex01_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
