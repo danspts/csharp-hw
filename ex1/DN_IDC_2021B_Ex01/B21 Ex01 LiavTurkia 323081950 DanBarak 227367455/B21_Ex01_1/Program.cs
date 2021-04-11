ac6eabdca46289918ce64d10cf1be0ea76cd3495
Dan Barak Sportes <danspts@gmail.com>
2021-04-12 00:29:20 +0300

    finished ex1

--- ex1/DN_IDC_2021B_Ex01/B21 Ex01 LiavTurkia 323081950 DanBarak 227367455/B21_Ex01_1/Program.cs
+++ ex1/DN_IDC_2021B_Ex01/B21 Ex01 LiavTurkia 323081950 DanBarak 227367455/B21_Ex01_1/Program.cs
@@ -7,18 +7,46 @@ namespace B21_Ex01_1
 
 		static int minNumber(int[] numbers)
         {
-			int minNumber = -1;
+			int minNumber = numbers[0];
 
+			for (int i = 1; i < numbers.Length; i++)
+			{
+				if (numbers[i] < minNumber)
+				{
+					minNumber = numbers[i];
+				}
+			}
 			return minNumber;
         }
 
 		static int maxNumber(int[] numbers)
 		{
-			int maxNumber = -1;
+			int maxNumber = numbers[0];
 
+			for(int i = 1; i < numbers.Length; i++)
+            {
+				if (numbers[i] > maxNumber)
+                {
+					maxNumber = numbers[i];
+				}
+			}
 			return maxNumber;
 
 		}
+
+		static int[] countNbOfOnes(string[] numbers)
+        {
+			int[] array_nbOfOnes = new int[3];
+
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				array_nbOfOnes[i] = countNbOfOnes(numbers[i]);
+
+			}
+
+			return array_nbOfOnes;
+		}
+
 
 		static int countNbOfOnes(string i_Digits)
 		{
@@ -48,10 +76,11 @@ namespace B21_Ex01_1
 		static int countPowersOfTwo(string[] numbers)
         {
 			int count = 0;
-			foreach(string number in numbers)
+			int nbOfOnes;
+			foreach (string number in numbers)
             {
-				int nbOfOnes = countNbOfOnes(number);
-				if (nbOfOnes < 2)
+				nbOfOnes = countNbOfOnes(number);
+				if (nbOfOnes == 1)
                 {
 					count++;
                 }
@@ -59,37 +88,87 @@ namespace B21_Ex01_1
 			return count;
         }
 
+		static int stringToInt(string str)
+        {
+			int result = 0;
+			foreach(char c in str)
+            {
+				result *= 2;
+				if(c == '1')
+                {
+					result++;
+                }
+            }
+			return result;
+        }
+
+		static bool strictlyMonotonicIncreasing(int i)
+        {
+			int prev = -1;
+			int next;
+			while(i > 0)
+            {
+				next = i % 10;
+				if (prev >= next)
+                {
+					return false;
+                }
+				i /= 10;
+				prev = next;
+            }
+			return true;
+        }
+
+		static int countStrictlyMonotonicIncreasing(int[] numbersInt)
+		{
+
+			int nbMonotonicInc = 0;
+
+			foreach (int num in numbersInt)
+			{
+				if (strictlyMonotonicIncreasing(num))
+				{
+					nbMonotonicInc++;
+				}
+			}
+
+			return nbMonotonicInc;
+		}
+
 		static void analyzeInput(string[] numbers)
 		{
 			object[] args = new object[9];
 
-			args[0] = numbers[0];
-			args[1] = numbers[1];
-			args[2] = numbers[2];
+			int[] numbersInt = new int[numbers.Length];
+
+			for(int i = 0; i < numbers.Length; i++)
+            {
+				numbersInt[i] = stringToInt(numbers[i]);
+				args[i] = numbersInt[i];
+			}
 
 			int nbPowersOfTwo = countPowersOfTwo(numbers);
 
 			args[3] = nbPowersOfTwo;
+			args[4] = countStrictlyMonotonicIncreasing(numbersInt);
 
 			int[] array_nbOfOnes = new int[3];
-
-			for (int i = 0; i < numbers.Length; i++)
-            {
-				array_nbOfOnes[i] = countNbOfOnes(numbers[i]);
-
-			}
-
+			array_nbOfOnes = countNbOfOnes(numbers);
+			
 			float averageNbOfOnes = average(array_nbOfOnes);
 			float averageNbOfZeros = 7 - averageNbOfOnes;
 
 			args[5] = averageNbOfOnes;
 			args[6] = averageNbOfZeros;
 
+			args[7] = maxNumber(numbersInt);
+			args[8] = minNumber(numbersInt);
+
 			StringBuilder sb = new StringBuilder();
 
-			sb.Append("The numbers are {0}, {1}, {2} - ");
+			sb.Append("\nThe numbers are {0}, {1}, {2} - ");
 			sb.Append("{3} of them is power of 2, ");
-			sb.Append("{4} of them consists of digits which are a strictly monotonically increasing sequence (i.e. 123), ");
+			sb.Append("{4} of them consists of digits which are a strictly monotonically increasing sequence, ");
 			sb.Append("the average of ones is {5}, ");
 			sb.Append("the average of zeros is {6}, ");
 			sb.Append("the greatest is {7}, ");
@@ -110,7 +189,7 @@ namespace B21_Ex01_1
 				string input = System.Console.ReadLine();
 				if (input.Length != 7)
 				{
-					System.Console.WriteLine("Illegal input: Must have 6 characters");
+					System.Console.WriteLine("Illegal input: Must have 7 characters");
 					return;
 				}
 				foreach (char c in input)
